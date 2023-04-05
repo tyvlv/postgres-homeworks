031d0ad99cd91b030e1a132cfa7f13d3c8479ccd
tychkov.vv <tyvl@yandex.ru>
2023-04-06 01:56:23 +0300

    Do hometask #1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+    customer_id varchar(5) PRIMARY KEY,
+    company_name varchar(100) NOT NULL,
+    contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE employees
+(
+    employee_id smallserial PRIMARY KEY,
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL,
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE orders
+(
+    order_id smallserial PRIMARY KEY,
+    customer_id varchar(100) REFERENCES customers(customer_id) NOT NULL,
+    employee_id smallserial REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,32 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+
+CUSTOMERS_DATA_PATH = 'north_data/customers_data.csv'
+EMPLOYEES_DATA_PATH = 'north_data/employees_data.csv'
+ORDERS_DATA_PATH = 'north_data/orders_data.csv'
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='123qwe')
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open(CUSTOMERS_DATA_PATH, 'r', encoding='utf8') as file:
+                reader = csv.DictReader(file)
+                for line in reader:
+                    cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", (line['customer_id'], line['company_name'], line['contact_name']))
+
+            with open(EMPLOYEES_DATA_PATH, 'r', encoding='utf8') as file:
+                reader = csv.DictReader(file)
+                employee_id = 0
+                for line in reader:
+                    employee_id += 1
+                    cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", (employee_id, line['first_name'], line['last_name'], line['title'], line['birth_date'], line['notes']))
+
+            with open(ORDERS_DATA_PATH, 'r', encoding='utf8') as file:
+                reader = csv.DictReader(file)
+                for line in reader:
+                    cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", (line['order_id'], line['customer_id'], line['employee_id'], line['order_date'], line['ship_city']))
+
+finally:
+    conn.close()
